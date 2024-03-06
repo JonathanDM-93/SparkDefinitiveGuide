c7bc597f976ae5c694667e783da2a67d671ca50b
Jonathan de la Rosa Maldonado <jonathan.rosa@totalplay.com.mx>
2024-03-05 22:00:20 -0600

    Se realizo la primera visualización del dataframe

--- EndtoEndExample.py
+++ EndtoEndExample.py
@@ -1,23 +1,13 @@
 # Ejemplo donde se carga un archivo csv
 
 # Cargar librerias
-
-from pyspark import SparkFiles
 from pyspark.sql import SparkSession
-
 
 # Crear sesión
 spark = SparkSession.builder.appName("EndtoEnd").getOrCreate()
 
-# El tipo de archivo PySpark puede leer otros formatos como: json, parquet, orc
-# Cargar el archivo que viene la ruta especificada en el libro
-
-# URL del archivo CSV en GitHub (enlace raw)
-url_csv = "https://raw.githubusercontent.com/databricks/Spark-The-Definitive-Guide/master/data/flight-data/csv/2015-summary.csv"
-
-# Descargar el archivo CSV localmente
-spark.sparkContext.addFile(url_csv)
-local_path = "file://" + SparkFiles.get("2015-summary.csv")
+# Ruta del archivo
+local_path = "C:/Users/joni_/Downloads/2015-summary.csv"
 
 # Cargar el conjunto de datos desde el archivo CSV local
 flight2015 = spark.read.format("csv") \
@@ -25,7 +15,22 @@ flight2015 = spark.read.format("csv") \
     .option("header", "true") \
     .load(local_path)
 
-flight2015.show(10, False)
-
 # Register the DataFrame as a SQL temporary view
 flight2015.createOrReplaceTempView("View")
+
+flight2015.show(10, False)
+# +-----------------+-------------------+-----+
+# |DEST_COUNTRY_NAME|ORIGIN_COUNTRY_NAME|count|
+# +-----------------+-------------------+-----+
+# |United States    |Romania            |15   |
+# |United States    |Croatia            |1    |
+# |United States    |Ireland            |344  |
+# |Egypt            |United States      |15   |
+# |United States    |India              |62   |
+# |United States    |Singapore          |1    |
+# |United States    |Grenada            |62   |
+# |Costa Rica       |United States      |588  |
+# |Senegal          |United States      |40   |
+# |Moldova          |United States      |1    |
+# +-----------------+-------------------+-----+
+# only showing top 10 rows
